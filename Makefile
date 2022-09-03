029f04f52dd67621a0c297a240fef6cd76565ebf
Simon Engledew <simon-engledew@github.com>
2022-09-03 09:55:59 +0100

    Use box leak

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 .PHONY: test
 test:
-	curl -v -X POST http://localhost:8080/validate/names -H 'Content-Type: application/json' -d '{}'
+	curl -v -X POST http://localhost:8080/schemas/names.json -H 'Content-Type: application/json' -d '{}'
--- src/main.rs
+++ src/main.rs
@@ -51,8 +51,8 @@ fn get_message(errors: jsonschema::ErrorIterator) -> String {
     out
 }
 
-#[post("/validate/{path}")]
-async fn validate(schemas: web::Data<HashMap<String, JSONSchema>>, document: web::Json<serde_json::Value>, info: web::Path<(String,)>) -> actix_web::Result<impl actix_web::Responder> {
+#[post("/{path:.*}")]
+async fn validate(schemas: web::Data<&'static HashMap<String, JSONSchema>>, document: web::Json<serde_json::Value>, info: web::Path<(String,)>) -> actix_web::Result<impl actix_web::Responder> {
     let info = info.into_inner();
 
     let schema = schemas.get(&info.0).ok_or(error::ErrorNotFound("not found"))?;
@@ -62,12 +62,11 @@ async fn validate(schemas: web::Data<HashMap<String, JSONSchema>>, document: web
     Ok("")
 }
 
-fn load_schemas(paths: impl IntoIterator<Item = String>) -> Result<HashMap<String, JSONSchema>, std::io::Error> {
+fn load_schemas(paths: impl IntoIterator<Item = String>) -> Result<&'static HashMap<String, JSONSchema>, std::io::Error> {
     let mut schemas = HashMap::new();
 
     for path_string in paths {
         let path = Path::new(&path_string);
-        let name = path.file_stem().and_then(|v|v.to_str()).ok_or(std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to process {}", path_string)))?;
 
         println!("Loading {}", path_string);
 
@@ -79,27 +78,57 @@ fn load_schemas(paths: impl IntoIterator<Item = String>) -> Result<HashMap<Strin
             .compile(&doc)
             .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to compile schema {}: {}", path_string, format_error(err))))?;
 
-        schemas.insert(String::from(name), schema);
+        schemas.insert(String::from(path.to_string_lossy()), schema);
     }
 
-    Ok(schemas)
+    Ok(Box::leak(Box::new(schemas)))
+}
+
+fn config(schemas: &'static HashMap<String, JSONSchema>) -> impl Fn(&mut web::ServiceConfig) {
+    move |cfg: &mut web::ServiceConfig| {
+        cfg
+            .app_data(web::Data::new(web::JsonConfig::default().limit(1024 * 1024 * 500)))
+            .app_data(web::Data::new(schemas))
+            .service(ping)
+            .service(validate);
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let schemas = web::Data::new(load_schemas(env::args().skip(1)).expect("Failed to load schemas"));
-
+    let schemas: &'static HashMap<String, JSONSchema> = load_schemas(env::args().skip(1)).expect("Failed to load schemas");
     let bind = env::var("ADDR").unwrap_or(String::from("127.0.0.1:8080"));
 
     println!("Listening on {}", bind);
 
-    HttpServer::new(move || App::new()
-            .app_data(web::Data::new(web::JsonConfig::default().limit(1024 * 1024 * 500)))
-            .app_data(schemas.clone())
-            .service(ping)
-            .service(validate)
-        )
+    HttpServer::new(move || App::new().configure(config(&schemas)))
         .bind(bind)?
         .run()
         .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{
+        http::{header::ContentType, StatusCode},
+        web::{Bytes},
+        test,
+    };
+
+    #[actix_web::test]
+    async fn test_validation() {
+        let schemas = load_schemas(vec![String::from("schemas/names.json")]).expect("ok");
+        let app = test::init_service(App::new().configure(config(&schemas))).await;
+        let req = test::TestRequest::post()
+            .insert_header(ContentType::json())
+            .uri("/schemas/names.json")
+            .set_payload("{}")
+            .to_request();
+
+        let resp = test::call_service(&app, req).await;
+        assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+        let result = test::read_body(resp).await;
+        assert_eq!(result, Bytes::from_static(b"\"name\" is a required property, \"age\" is a required property"));
+    }
 }
